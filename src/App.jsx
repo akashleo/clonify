cd950e687c903edf9b51df47edcca7baea259000
Akash Ghosh <akashleo2009@gmail.com>
2023-01-01 23:36:39 +0530

    Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useSelector } from 'react-redux';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+//import { useSelector } from 'react-redux';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 // import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
 // import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
 
 const App = () => {
-  const { activeSong } = useSelector((state) => state.player);
-
   return (
     <BrowserRouter>
       <Box sx={{ backgroundColor: '#000' }}>
         Navbar
+        <Routes>
+          <Router path="/" exact element={<Feed/>} />
+          <Router path="/video/:id" exact element={<VideoDetail/>}/>
+          <Router path="/channel/:id" exact element={<ChannelDetail/>}/>
+          <Router path="/search/:searchTerm" exact element={<SearchFeed/>}/>
+        </Routes>
       </Box>
     </BrowserRouter>
   );
